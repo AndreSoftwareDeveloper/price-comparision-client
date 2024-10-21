2a75a6de4cb4a215404ee0848c47bd22859b0585
YoungNavigator <andriej2301@gmail.com>
2024-10-22 00:33:51 +0200

    feat: Component for activating an account

--- src/app/account-activation/account-activation.component.ts
+++ src/app/account-activation/account-activation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-account-activation',
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './account-activation.component.scss'
 })
 export class AccountActivationComponent {
-  
+  token: string = ""
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.token = params.get('token') ?? "";
+      let token_exists = this.apiService.checkActivationToken(this.token)
+      console.log(token_exists)
+    })
+  }
 }
