824917dc46f99578edea8d44ee683f3c0dc3aa7a
YoungNavigator <andriej2301@gmail.com>
2024-10-22 00:49:34 +0200

    fix: Redirect to home page if no subpage is specified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'account-activation', component: AccountActivationComponent }
 ];
